429f348ff4e4c3fabe29c55508756fd9e63257da
Alessandro Luiz dos Santos <aleluizsantos@gmail.com>
2023-10-08 10:28:52 -0300

    Create Thema

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,18 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import { Header } from "./components/header";
 import { Aside } from "./components/aside";
 import { Section } from "./components/section";
 
 export function App() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1 p-6 flex gap-6">
-        <Section />
-        <Aside />
-      </main>
-    </div>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1 p-6 flex gap-6">
+          <Section />
+          <Aside />
+        </main>
+      </div>
+    </ThemeProvider>
   );
 }
